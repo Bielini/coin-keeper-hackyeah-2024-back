a6d610bbe9c6453a6283df0c532dc74c263ae033
Piotr Bielawski <piotr.bielawski@epoka.io>
2024-09-28 10:42:30 +0200

    initial commit

---
+++ src/main/java/cap/hackyeah/coin_keeper_hackyeah_2024_back/CoinKeeperHackyeah2024BackApplication.java
@@ -0,0 +1,13 @@
+package cap.hackyeah.coin_keeper_hackyeah_2024_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinKeeperHackyeah2024BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinKeeperHackyeah2024BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cap/hackyeah/coin_keeper_hackyeah_2024_back/CoinKeeperHackyeah2024BackApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.hackyeah.coin_keeper_hackyeah_2024_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinKeeperHackyeah2024BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
